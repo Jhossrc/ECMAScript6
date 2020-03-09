0d7598cb77c994df5d5f1878d20eddd89801ad7b
Jhoss <kuro.rc10@gmail.com>
2020-03-09 16:21:43 -0500

    Creando los archivos necesarios para empezar el curso y viendo un poco sobre los parametros por defecto

---
+++ src/es6/index.js
@@ -0,0 +1,18 @@
+// Parametros por defecto
+
+function newFunction(name, age, country) {
+    var name = name || 'Jose';
+    var age = age || 21;
+    var country = country || 'PE';
+
+    console.log(name, age, country);
+}
+
+//es6
+
+function newFunction2(name = 'Jose', age = 21, country = 'PE') {
+    console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('Ricardo', '23', 'CO')
