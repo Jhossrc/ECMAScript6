9d9cb24a5434a09c1fdc8c3fd5a2b4b6daca272a
Jhoss <kuro.rc10@gmail.com>
2020-03-10 11:17:07 -0500

    Async - await

--- src/es8/index.js
+++ src/es8/index.js
@@ -27,3 +27,31 @@ console.log(values);
 const string = 'hello';
 console.log(string.padStart(7, 'fff'));
 console.log(string.padEnd(12, '---fsafas--'))
+
+// Async - await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true) ?
+        setTimeout(() => resolve('Hello World'), 3000): reject(new Error('Test Error'))
+
+    })
+}
+
+const helloAsync = async() => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async() => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
