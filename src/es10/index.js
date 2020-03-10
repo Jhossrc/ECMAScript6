fa168a3ae687d04171c0f0f680ec6d392e4faa2c
Jhoss <kuro.rc10@gmail.com>
2020-03-10 12:07:07 -0500

    trimStart() - trimEnd()

--- src/es10/index.js
+++ src/es10/index.js
@@ -14,3 +14,14 @@ let array = [1, 2, 3, 4, 5];
 
 console.log(array.flatMap(value => [value, value * 2]));
 // [1,2,  2,4,  3,6,  4,8,  5,10]
+
+
+let hello = '           hello world';
+console.log(hello);
+
+console.log(hello.trimStart());
+
+let hello = 'hello world        ';
+console.log(hello);
+
+console.log(hello.trimEnd());
