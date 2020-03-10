07ab3e08503e8c84184aef97bc7eafdf5a26a7a7
Jhoss <kuro.rc10@gmail.com>
2020-03-10 10:18:21 -0500

    Object.values

--- src/es8/index.js
+++ src/es8/index.js
@@ -10,3 +10,14 @@ const entries = Object.entries(data);
 console.log(entries);
 
 console.log(entries.length);
+
+// Object,values
+
+const data2 = {
+    frontend: "Oscar",
+    backend: "Isabel",
+    design: "Ana"
+};
+
+const values = Object.values(data2);
+console.log(values);
