4cd674cfedf24600ebc3fdfeb0e8b3106a57f083
Jhoss <kuro.rc10@gmail.com>
2020-03-10 10:16:27 -0500

    ES8 Object.entries()

---
+++ src/es8/index.js
@@ -0,0 +1,12 @@
+// Object.entries
+
+const data = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+
+console.log(entries.length);
